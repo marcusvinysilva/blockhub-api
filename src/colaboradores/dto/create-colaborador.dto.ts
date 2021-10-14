9944edeccd91a37ed480403d4394a85c0eaa1bba
Marcus <mvsilva1997@gmail.com>
2021-10-14 01:10:23 -0300

    add swagger config on dto

--- src/colaboradores/dto/create-colaborador.dto.ts
+++ src/colaboradores/dto/create-colaborador.dto.ts
@@ -1,18 +1,24 @@
 import { IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateColaboradorDto {
+  @ApiProperty()
   @IsNotEmpty()
   nome: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   cargo: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   admissao: string;
 
+  @ApiProperty()
   @IsOptional()
   ativo: boolean;
 
+  @ApiProperty()
   @IsOptional()
   projetos: number[];
 }
--- src/projetos/dto/create-projeto.dto.ts
+++ src/projetos/dto/create-projeto.dto.ts
@@ -1,21 +1,28 @@
 import { IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProjetoDto {
+  @ApiProperty()
   @IsNotEmpty()
   nome: string;
 
+  @ApiProperty()
   @IsOptional()
   descricao: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   inicio: string;
 
+  @ApiProperty()
   @IsOptional()
   fim: string;
 
+  @ApiProperty()
   @IsOptional()
   ativo: boolean;
 
+  @ApiProperty()
   @IsOptional()
   colaboradores: number[];
 }
